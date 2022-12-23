bf30ccee1cff9741ec5b38c6b44fbc97818a0d6b
SEC-MASUM <masum.jess42@gmail.com>
2022-12-23 19:58:42 +0600

    updated way to use react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { BrowserRouter, Link, NavLink, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+// layouts
+import RootLayout from "./layouts/RootLayout";
 // pages
 import About from "./pages/About";
 import Home from "./pages/Home";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About Us</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
